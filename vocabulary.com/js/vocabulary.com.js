bdff303d862e118a6e20162da3d7c87a29c7459b
Ethan Yu <sohbmpethan@gmail.com>
2024-04-15 15:05:51 +0900

    feat(vocabulary.com): add script for Vocabulary.com

---
+++ vocabulary.com/js/vocabulary.com.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Vocabulary.com Enhancements
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Enhance vocabulary.com with custom styles
+// @author       Ethan Yu
+// @match        *://*.vocabulary.com/*
+// @icon         https://www.google.com/s2/favicons?domain=vocabulary.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // Update the CSS URL if necessary to point to a stylesheet applicable for vocabulary.com
+    const cssURL = 'https://raw.githubusercontent.com/0x00000024/my-tampermonkey-script/main/vocabulary.com/build/stylesheets/vocabulary.com.css';
+
+    console.debug('start: add CSS');
+
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: cssURL,
+        onload: function (response) {
+            GM_addStyle(response.responseText);
+            console.debug('done: add CSS');
+        }
+    });
+})();
